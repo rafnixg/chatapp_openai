7bbc25940f92f729a86de7f25e550841d6856985
Rafnix Guzmán <rafnixg@gmail.com>
2023-03-15 18:30:37 -0500

    IMP: Change model for gpt-3.5-turbo

--- worker.py
+++ worker.py
@@ -7,10 +7,17 @@ openai.api_key = os.environ.get("OPENAI_API_KEY", None)
 
 def openai_process_message(user_message):
     # Set the prompt for OpenAI Api
-    prompt = "\"Act like a personal assistant. You can respond to questions, translate sentences, summarize news, and give recommendations. " + user_message + "\""
+    prompt = [
+        {
+            "role": "system",
+            "content": "Act like a personal assistant. You can respond to questions, translate sentences, summarize news, and give recommendations."
+        },
+        {"role": "user", "content": user_message}
+    ]
     # Call the OpenAI Api to process our prompt
-    openai_response = openai.Completion.create(model="text-davinci-003", prompt=prompt,max_tokens=4000)
+    openai_response = openai.Completion.create(model="gpt-3.5-turbo", prompt=prompt, max_tokens=2000)
+    
     print("openai response:", openai_response)
     # Parse the response to get the response text for our prompt
-    response_text = openai_response.choices[0].text
+    response_text = openai_response.choices[0].message.content
     return response_text
